51c82f6904d538c6df57a6b6cdd5061a715dfb2a
suotas <accf8b33@gmail.com>
2020-12-11 20:04:19 +0900

    mod7 clear

--- skillcheck_sample/mod7/main.go
+++ skillcheck_sample/mod7/main.go
@@ -14,21 +14,58 @@ func main() {
 	// nの読み込み
 	n := nextInt()
 
-	var input []int
+	// 各a_iについて、7で割った余りの出現回数を数える
+	counts := make([]int, 7)
 	for range make([]int, n) {
-		input = append(input, nextInt())
+		counts[nextInt()%7]++
 	}
 
-	// カードの組み合わせを作る
-	combination := getAllConbinationAt3(input)
-
-	// 配列ごとに合計値が7で割り切れるかチェック
+	// 7で割り切れる組み合わせの総数
 	sum := 0
-	for _, v := range combination {
-		sum += isMod7(v)
+
+	// 0-6の数字から1つ目の数字を選ぶループ
+	for i := 0; i < 7; i++ {
+		// 0-6の数字から2つ目の数字を選ぶループ
+		for j := i; j < 7; j++ {
+			// 0-6の数字から3つ目の数字を選ぶループ
+			for k := j; k < 7; k++ {
+				// 3つの数字の和が7で割り切れない場合、カウントしない
+				if (i+j+k)%7 != 0 {
+					continue
+				}
+
+				// 3つの数字の出現回数を掛け合わせる
+				c1 := counts[i]
+				c2 := counts[j]
+				c3 := counts[k]
+
+				// 2つの数字が同じ場合、重複を除く為
+				// 片方から1を引いて積を2で割る
+				// 3つの数字が同じ場合、重複を除く為
+				// 片方から1を、もう片方から2を引いて積を6で割る
+				if i == j {
+					c2 -= 1
+				}
+				if k == i {
+					c3 -= 1
+				}
+				if k == j {
+					c3 -= 1
+				}
+
+				pat := c1 * c2 * c3
+				if i == j && j == k {
+					pat /= 6
+				} else if i == j || i == k || j == k {
+					pat /= 2
+				}
+
+				sum += pat
+			}
+		}
 	}
 
-	// 7で割り切れた配列の数を出力
+	// 7で割り切れた組み合わせの数を出力
 	fmt.Println(sum)
 }
 
@@ -39,31 +76,4 @@ func nextInt() int {
 		panic(e)
 	}
 	return i
-}
-
-func getAllConbinationAt3(card []int) [][]int {
-
-	var result [][]int
-
-	for i := 0; i < len(card); i++ {
-		for j := i + 1; j < len(card); j++ {
-			for k := j + 1; k < len(card); k++ {
-				result = append(result, []int{card[i], card[j], card[k]})
-			}
-		}
-	}
-
-	return result
-}
-
-func isMod7(arr []int) int {
-	sum := 0
-	for _, v := range arr {
-		sum += v
-	}
-	if sum%7 == 0 {
-		return 1
-	} else {
-		return 0
-	}
 }
